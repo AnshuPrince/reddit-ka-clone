d0c1968f9c862972e1b4425240070e5962be7939
Anshu Prince <anshu.prince@jungleworks.com>
2018-10-29 19:08:56 +0530

    reddit clone v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Article } from './article/article.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reddit-ka-clone';
+  articles : Article[];
+  constructor(){
+    this.articles = [
+      new Article( 'Angular 2','https://www.angular.io',25),
+      new Article( 'FullStack','https://www.fullstack.io',50)
+    ];
+  }
+  addArticle(title:HTMLInputElement, link:HTMLInputElement) : boolean{
+    this.articles.push(new Article(title.value,link.value));
+    title.value = '';
+    link.value = '';
+  return false;
+  }
+  sortedArticles() : Article[]{
+    return this.articles.sort((a : Article, b : Article) => b.votes - a.votes);
+  }
 }
